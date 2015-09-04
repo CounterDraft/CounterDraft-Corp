a258f193f85d9f44e1c6f0d067949da003075a35
Jerum Hubbert <hubbertj@gmail.com>
2015-09-04 12:58:37 -0700

    as-1000
    Added configuration check in the index.js script.

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
+console.log('Running Project....');
 var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
+var npm = require("npm");
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+var loadServer = function() {
+    app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname));
+    app.use(express.static(__dirname));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+    // views is directory for all template files
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
 
-// defaults to 'html_corp' if not defined in the render();
-app.set('layout', 'layouts/html_corp'); 
+    // defaults to 'html_corp' if not defined in the render();
+    app.set('layout', 'layouts/html_corp');
 
-app.use(expressLayouts);
+    app.use(expressLayouts);
 
-app.get('/', function(request, response) {
-    response.render('pages/home');
-}).get('/about', function(request, response) {
-    response.render('pages/about');
-}).get('/meet-the-team', function(request, response) {
-    response.render('pages/meet-the-team');
-}).get('/contact', function(request, response) {
-    response.render('pages/contact');
-});
+    app.get('/', function(request, response) {
+        response.render('pages/home');
+    }).get('/about', function(request, response) {
+        response.render('pages/about');
+    }).get('/meet-the-team', function(request, response) {
+        response.render('pages/meet-the-team');
+    }).get('/contact', function(request, response) {
+        response.render('pages/contact');
+    }).get('/faq', function(request, response) {
+        response.render('pages/faq');
+    });
 
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
+    app.listen(app.get('port'), function() {
+        console.log('Node app is running on port', app.get('port'));
+    });
+
+}
+
+var runDev = function(){
+	console.log('Updateing packages, please wait...');
+    npm.load({
+        "dev": true
+    }, function(err) {
+        // catch errors
+        npm.commands.install(function(er, data) {
+        	if(er){
+        		console.log('Failed to update packages ' + er);
+        	}
+            loadServer();
+        });
+        npm.on("log", function(message) {
+            console.log(message);
+        });
+    });
+}
+
+//if we are running in dev mode we update npm;
+if (process.env.MODE === 'development') {
+	runDev();
+} else if(process.env.MODE === 'production') {
+	loadServer();
+}else{
+	console.log('MODE not set, ex. MODE=development or MODE=production');
+	console.log('Running in development mode.');
+	runDev();
+}
