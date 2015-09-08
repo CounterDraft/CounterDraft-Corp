c95ccf1bd16b0ff2e7658ac90214953330ffdb37
Jerum Hubbert <hubbertj@gmail.com>
2015-09-08 11:22:51 -0700

    Added new look and feel to the button and image to the home screen.

---
+++ js/base/view.js
@@ -0,0 +1,26 @@
+// requirejs.config({
+// 	baseUrl: "/js/",
+// 	waitSeconds: 12,
+// 	paths: {
+// 		lib: 	"./lib",
+// 		home: 	"./",
+// 		main: 	"./main",
+// 		async: 	"./async",
+// 		modules:"./modules"
+// 	}
+// });
+
+Counter = {
+    Navigation: {
+    	set: function(title){
+    		$('ul.navbar-nav li').removeClass('active');
+    		$('ul.navbar-nav li.' + title +'-nav').addClass('active');
+    		//add other navgation items if needed.
+    	}
+
+    }
+}
+
+ga = function(){
+	console.log('loaded' + arguments);
+}
