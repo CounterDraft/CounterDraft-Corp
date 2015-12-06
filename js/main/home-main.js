3d2d04eb0fd6fdf67bc76c1a4d90087273ccacb9
Jerum Hubbert <hubbertj@gmail.com>
2015-12-05 18:00:33 -0800

    MI-0107 Added functionality for dialog/modal. TODO: Do the styling for it.

--- js/main/home-main.js
+++ js/main/home-main.js
@@ -1,13 +1,32 @@
 "use strict";
-if(App){
-	window.App.controller('homeController', ['$scope', function($scope) {
-		var self = this;
+if (App) {
+    window.App.controller('homeController', ['$http', function($http) {
+        var self = this;
+        self.isModel = false;
+        self.user = {};
+        self.NewsletterModel = "/views/templates/home/modal/newsletter-modal.html"
 
-		self.showDialog = function(){
-			alert('this works');
-			return false;
-		}
-	}]);
-}else{
-	console.error('Failed to find App object.');
+        var dismissModal = function() {
+            $('#newsletter-modal').modal('hide');
+            self.isModel = false;
+        }
+
+        self.showDialog = function() {
+            self.isModel = true;
+            return false;
+        }
+        self.nwSubmit = function() {
+            var ifInformationIsSubmitted = true;
+            alert("Infomation to be saved - " + JSON.stringify(self.user));
+
+            if (ifInformationIsSubmitted) {
+            	dismissModal();
+            }
+
+            return false;
+        }
+
+    }]);
+} else {
+    console.error('Failed to find App object.');
 }
