5a11db65ffeb9fbe65cd3bd8192a32fc6527baf4
Jerum Hubbert <hubbertj@gmail.com>
2016-01-14 20:17:27 -0800

    MI-0102: Completed saving information to the backend of base.

--- index.js
+++ index.js
@@ -38,12 +38,18 @@ var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
 var npm = require("npm");
 var app = express();
+var bodyParser = require('body-parser');
 
 var router = require('./app/corp-router');
 
 
 var launchApp = function() {
     app.use(express.static(__dirname));
+
+    app.use(bodyParser.json()); // to support JSON-encoded bodies
+    app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+        extended: true
+    }));
 
     // views is directory for all template files
     app.set('views', __dirname + '/views');
--- js/main/home-main.js
+++ js/main/home-main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 angular.module('app-counterdraft')
-    .controller('homeController', ['$http', 'counterDraft-message-srv', function($http, messageService) {
+    .controller('homeController', ['$http', '$timeout', 'counterDraft-message-srv', function($http, $timeout, messageService) {
         var self = this;
 
         var msgElement = document.querySelector('message');
@@ -22,118 +22,68 @@ angular.module('app-counterdraft')
         }
         self.nwSubmit = function() {
 
+            self.user['name'] = self.user.first_name + " " + self.user.last_name;
+            self.user.address['postal_code'] = self.user.address['postal_code'].toString(); 
+
+            console.log(self.user);
+
+            var outPutData = {
+                "data": self.user
+            };
+
             var testData = {
-                "items": [{
-                    "data": {
-                        "id": 1,
-                        "creator_id": 1,
-                        "owner_id": 1,
-                        "is_organization": true,
-                        "contact_id": null,
-                        "name": "Design Services Company",
-                        "first_name": null,
-                        "last_name": null,
-                        "customer_status": "none",
-                        "prospect_status": "none",
-                        "title": null,
-                        "description": null,
-                        "industry": "Design Services",
-                        "website": "http://www.designservice.com",
-                        "email": null,
-                        "phone": null,
-                        "mobile": null,
-                        "fax": "+44-208-1234567",
-                        "twitter": null,
-                        "facebook": null,
-                        "linkedin": null,
-                        "skype": null,
-                        "address": {
-                            "line1": "2726 Smith Street",
-                            "city": "Hyannis",
-                            "postal_code": "02601",
-                            "state": "MA",
-                            "country": "US"
-                        },
-                        "tags": [
-                            "important"
-                        ],
-                        "custom_fields": {
-                            "known_via": "tom"
-                        },
-                        "created_at": "2014-08-27T16:32:56Z",
-                        "updated_at": "2014-08-27T16:32:56Z"
+                "data": {
+                    "contact_id": 1,
+                    "name": "JJ Johnson",
+                    "first_name": "JJ",
+                    "last_name": "Johnson",
+                    "title": "CEO",
+                    "description": "I know him via Tom",
+                    "industry": "Design Services",
+                    "website": "http://www.designservice.com",
+                    "email": "mark@designservices.com",
+                    "phone": "508-778-6516",
+                    "mobile": "508-778-6516",
+                    "fax": "+44-208-1234567",
+                    "twitter": "mjohnson",
+                    "facebook": "mjohnson",
+                    "linkedin": "mjohnson",
+                    "skype": "mjohnson",
+                    "address": {
+                        "line1": "2726 Smith Street",
+                        "city": "Hyannis",
+                        "postal_code": "02601",
+                        "state": "MA",
+                        "country": "US"
                     },
-                    "meta": {
-                        "type": "contact"
-                    }
-                }, {
-                    "data": {
-                        "id": 2,
-                        "creator_id": 1,
-                        "owner_id": 1,
-                        "is_organization": false,
-                        "contact_id": 1,
-                        "name": "Mark Johnson",
-                        "first_name": "Mark",
-                        "last_name": "Johnson",
-                        "customer_status": "none",
-                        "prospect_status": "none",
-                        "title": "CEO",
-                        "description": "I know him via Tom",
-                        "industry": "Design Services",
-                        "website": "http://www.designservice.com",
-                        "email": "mark@designservices.com",
-                        "phone": "508-778-6516",
-                        "mobile": "508-778-6516",
-                        "fax": "+44-208-1234567",
-                        "twitter": "mjohnson",
-                        "facebook": "mjohnson",
-                        "linkedin": "mjohnson",
-                        "skype": "mjohnson",
-                        "address": {
-                            "line1": "2726 Smith Street",
-                            "city": "Hyannis",
-                            "postal_code": "02601",
-                            "state": "MA",
-                            "country": "US"
-                        },
-                        "tags": [
-                            "important"
-                        ],
-                        "custom_fields": {
-                            "known_via": "tom"
-                        },
-                        "created_at": "2014-08-27T16:32:56Z",
-                        "updated_at": "2014-08-27T16:32:56Z"
-                    },
-                    "meta": {
-                        "type": "contact"
-                    }
-                }],
-                "meta": {
-                    "type": "collection",
-                    "count": 2,
-                    "links": {
-                        "self": "http://api.getbase.com/v2/contacts"
+                    "tags": [
+                        "contractor",
+                        "early-adopter"
+                    ],
+                    "custom_fields": {
+                        "referral_website": "http://www.example.com"
                     }
                 }
             }
 
-
             $http({
                 method: 'POST',
                 url: '/v2/contacts',
-                data: self.user
+                data: outPutData
             }).then(function successCallback(res) {
                 messageService.setElement(document.querySelector('message'));
-                console.log(JSON.stringify(res));
-                dismissModal();
+                messageService.sendMessage('Thank you for signing up for the CounterDraft newsletter.', 'success');
+
+                $timeout(function() {
+                    dismissModal();
+                }, 3000);
+
             }, function errorCallback(res) {
                 messageService.setElement(document.querySelector('message'));
                 console.log(JSON.stringify(res));
-                if(res.data.error && res.data.error.length > 0){
+                if (res.data.error && res.data.error.length > 0) {
                     messageService.sendMessage(res.data.error[0].msg, 'danger');
-                }else{
+                } else {
                     messageService.sendMessage('We failed to save your information, please try again.', 'danger');
                 }
             });
